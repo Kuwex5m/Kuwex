f3d6bf67116b496e98fefc6761c4d95a1dc259bc
Kuwex5m <kuwexcom@gmail.com>
2025-11-10 22:52:40 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,8 +1,6 @@
 // firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-storage.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore, doc, getDoc, setDoc, updateDoc } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyB4yjiRg1GTfS54Z0Z6J0KiFCgo2U_zItU",
@@ -13,7 +11,8 @@ const firebaseConfig = {
   appId: "1:5986214795:web:e68bff5ec8af2e1bcb858e"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+const db = getFirestore(app);
+
+export { db, doc, getDoc, setDoc, updateDoc };
